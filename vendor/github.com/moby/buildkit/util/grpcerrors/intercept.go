edfa99a4cd87fd9d75c0e6103d42029aec54abde
Mathew <mat999@gmail.com>
2025-06-02 21:21:10 +1000

    remove buildkit stacks
    Update intercept.go

--- vendor/github.com/moby/buildkit/util/grpcerrors/intercept.go
+++ vendor/github.com/moby/buildkit/util/grpcerrors/intercept.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"os"
 
-	"github.com/moby/buildkit/util/stack"
 	"github.com/pkg/errors"
 	"google.golang.org/grpc"
 )
@@ -14,7 +13,6 @@ func UnaryServerInterceptor(ctx context.Context, req interface{}, info *grpc.Una
 	resp, err = handler(ctx, req)
 	oldErr := err
 	if err != nil {
-		stack.Helper()
 		err = ToGRPC(err)
 	}
 	if oldErr != nil && err == nil {
@@ -31,24 +29,15 @@ func UnaryServerInterceptor(ctx context.Context, req interface{}, info *grpc.Una
 
 func StreamServerInterceptor(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
 	err := ToGRPC(handler(srv, ss))
-	if err != nil {
-		stack.Helper()
-	}
 	return err
 }
 
 func UnaryClientInterceptor(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
 	err := FromGRPC(invoker(ctx, method, req, reply, cc, opts...))
-	if err != nil {
-		stack.Helper()
-	}
 	return err
 }
 
 func StreamClientInterceptor(ctx context.Context, desc *grpc.StreamDesc, cc *grpc.ClientConn, method string, streamer grpc.Streamer, opts ...grpc.CallOption) (grpc.ClientStream, error) {
 	s, err := streamer(ctx, desc, cc, method, opts...)
-	if err != nil {
-		stack.Helper()
-	}
 	return s, ToGRPC(err)
 }
