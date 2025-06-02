68413a6796a86f01411e314337b8f1b3ae4903ae
Mathew <mat999@gmail.com>
2025-06-02 12:15:00 +1000

    reduce initializer strings built

--- vendor/github.com/docker/distribution/reference/regexp.go
+++ vendor/github.com/docker/distribution/reference/regexp.go
@@ -2,7 +2,6 @@ package reference
 
 import (
 	"regexp"
-	"strings"
 )
 
 // DomainRegexp matches hostname or IP-addresses, optionally including a port
@@ -14,7 +13,7 @@ import (
 // addresses such as IPv4-Mapped).
 //
 // [RFC 6874]: https://www.rfc-editor.org/rfc/rfc6874.
-var AnchoredDomainRegexp =  "^" + domainAndPort + "$"
+var AnchoredDomainRegexp = "^" + domainAndPort + "$"
 
 // NameRegexp is the format for the name component of references, including
 // an optional domain and port, but without tag or digest suffix.
@@ -57,7 +56,8 @@ const (
 	optionalPort = `(?::[0-9]+)?`
 
 	// tag matches valid tag names. From docker/docker:graph/tags.go.
-	tag = `[\w][\w.-]{0,127}`
+	tag         = `[\w][\w.-]{0,127}`
+	anchoredTag = `^` + tag + `$`
 
 	// digestPat matches well-formed digests, including algorithm (e.g. "sha256:<encoded>").
 	//
@@ -70,25 +70,25 @@ const (
 	//     in [rfc4648, section 5].
 	//
 	// [rfc4648, section 5]: https://www.rfc-editor.org/rfc/rfc4648#section-5.
-	digestPat = `[A-Za-z][A-Za-z0-9]*(?:[-_+.][A-Za-z][A-Za-z0-9]*)*[:][[:xdigit:]]{32,}`
+	digestPat         = `[A-Za-z][A-Za-z0-9]*(?:[-_+.][A-Za-z][A-Za-z0-9]*)*[:][[:xdigit:]]{32,}`
+	anchoredDigestPat = `^` + digestPat + `$`
 
 	// identifier is the format for a content addressable identifier using sha256.
 	// These identifiers are like digests without the algorithm, since sha256 is used.
-	identifier = `([a-f0-9]{64})`
+	identifier         = `([a-f0-9]{64})`
+	anchoredIdentifier = `^` + identifier + `$`
 
 	// ipv6address are enclosed between square brackets and may be represented
 	// in many ways, see rfc5952. Only IPv6 in compressed or uncompressed format
 	// are allowed, IPv6 zone identifiers (rfc6874) or Special addresses such as
 	// IPv4-Mapped are deliberately excluded.
 	ipv6address = `\[(?:[a-fA-F0-9:]+)\]`
-)
 
-var (
 	// domainName defines the structure of potential domain components
 	// that may be part of image names. This is purposely a subset of what is
 	// allowed by DNS to ensure backwards compatibility with Docker image
 	// names. This includes IPv4 addresses on decimal format.
-	domainName = domainNameComponent + anyTimes(`\.`+domainNameComponent)
+	domainName = domainNameComponent + `(?:` + `\.` + domainNameComponent + `)*`
 
 	// host defines the structure of potential domains based on the URI
 	// Host subcomponent on rfc3986. It may be a subset of DNS domain name,
@@ -99,57 +99,40 @@ var (
 
 	// allowed by the URI Host subcomponent on rfc3986 to ensure backwards
 	// compatibility with Docker image names.
-	domainAndPort =host + optionalPort
-
-	// anchoredTagRegexp matches valid tag names, anchored at the start and
-	// end of the matched string.
-	anchoredTagRegexp = regexp.MustCompile(anchored(tag))
-
-	// anchoredDigestRegexp matches valid digests, anchored at the start and
-	// end of the matched string.
-	anchoredDigestRegexp = regexp.MustCompile(anchored(digestPat))
+	domainAndPort = host + optionalPort
 
 	// pathComponent restricts path-components to start with an alphanumeric
 	// character, with following parts able to be separated by a separator
 	// (one period, one or two underscore and multiple dashes).
-	pathComponent = alphanumeric + anyTimes(separator+alphanumeric)
+	pathComponent = alphanumeric + "(?:" + (separator + alphanumeric) + ")*"
 
 	// remoteName matches the remote-name of a repository. It consists of one
 	// or more forward slash (/) delimited path-components:
 	//
 	//	pathComponent[[/pathComponent] ...] // e.g., "library/ubuntu"
-	remoteName = pathComponent + anyTimes(`/`+pathComponent)
-	namePat    = optional(domainAndPort+`/`) + remoteName
+	remoteName = pathComponent + "(" + `/` + pathComponent + ")*"
+	namePat    = "(?:" + domainAndPort + `/` + ")?" + remoteName
+
+	anchoredName = "^(?:(" + domainAndPort + ")/(" + remoteName + "))?$"
+
+	referencePat = "^(" + namePat + ")(?:" + `:(` + tag + "))?(?:" + `@(` + digestPat + "))?$"
+)
+
+var (
+
+	// anchoredTagRegexp matches valid tag names, anchored at the start and
+	// end of the matched string.
+	anchoredTagRegexp = regexp.MustCompile(anchoredTag)
+
+	// anchoredDigestRegexp matches valid digests, anchored at the start and
+	// end of the matched string.
+	anchoredDigestRegexp = regexp.MustCompile(anchoredDigestPat)
 
 	// anchoredNameRegexp is used to parse a name value, capturing the
 	// domain and trailing components.
-	anchoredNameRegexp = regexp.MustCompile(anchored(optional(capture(domainAndPort), `/`), capture(remoteName)))
-
-	referencePat = anchored(capture(namePat), optional(`:`, capture(tag)), optional(`@`, capture(digestPat)))
+	anchoredNameRegexp = regexp.MustCompile(anchoredName)
 
 	// anchoredIdentifierRegexp is used to check or match an
 	// identifier value, anchored at start and end of string.
-	anchoredIdentifierRegexp = regexp.MustCompile(anchored(identifier))
+	anchoredIdentifierRegexp = regexp.MustCompile(anchoredIdentifier)
 )
-
-// optional wraps the expression in a non-capturing group and makes the
-// production optional.
-func optional(res ...string) string {
-	return `(?:` + strings.Join(res, "") + `)?`
-}
-
-// anyTimes wraps the expression in a non-capturing group that can occur
-// any number of times.
-func anyTimes(res ...string) string {
-	return `(?:` + strings.Join(res, "") + `)*`
-}
-
-// capture wraps the expression in a capturing group.
-func capture(res ...string) string {
-	return `(` + strings.Join(res, "") + `)`
-}
-
-// anchored anchors the regular expression by adding start and end delimiters.
-func anchored(res ...string) string {
-	return `^` + strings.Join(res, "") + `$`
-}
