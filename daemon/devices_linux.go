dab3b21892e49ee2d7d5e41763634d35b121dbf9
Mathew <mat999@gmail.com>
2025-06-03 09:19:44 +1000

    remove device driver support

--- daemon/devices_linux.go
+++ daemon/devices_linux.go
@@ -6,8 +6,6 @@ import (
 	specs "github.com/opencontainers/runtime-spec/specs-go"
 )
 
-var deviceDrivers = map[string]*deviceDriver{}
-
 type deviceDriver struct {
 	capset     capabilities.Set
 	updateSpec func(*specs.Spec, *deviceInstance) error
@@ -18,21 +16,6 @@ type deviceInstance struct {
 	selectedCaps []string
 }
 
-func registerDeviceDriver(name string, d *deviceDriver) {
-	deviceDrivers[name] = d
-}
-
 func (daemon *Daemon) handleDevice(req container.DeviceRequest, spec *specs.Spec) error {
-	if req.Driver == "" {
-		for _, dd := range deviceDrivers {
-			if selected := dd.capset.Match(req.Capabilities); selected != nil {
-				return dd.updateSpec(spec, &deviceInstance{req: req, selectedCaps: selected})
-			}
-		}
-	} else if dd := deviceDrivers[req.Driver]; dd != nil {
-		if selected := dd.capset.Match(req.Capabilities); selected != nil {
-			return dd.updateSpec(spec, &deviceInstance{req: req, selectedCaps: selected})
-		}
-	}
 	return incompatibleDeviceRequest{req.Driver, req.Capabilities}
 }
